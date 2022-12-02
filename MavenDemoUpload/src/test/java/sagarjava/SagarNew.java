919d2193cb8e8ae0612dbbe335865343eb61995f
sagar Gidwani <sagar Gidwani@Sagar>
2022-12-02 10:39:43 +0530

    done by sa

---
+++ MavenDemoUpload/src/test/java/sagarjava/SagarNew.java
@@ -0,0 +1,12 @@
+package sagarjava;
+
+public class SagarNew {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sagar ");
+		System.out.println("nasim ");
+		System.out.println("Jignesh ");
+	}
+
+}
