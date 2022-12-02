4f6e76852f8cbf2bb5ceafff7668ad58bc20c55e
Sunil Chauhan <Sunil Chauhan@Sunil-Chauhan>
2022-12-02 14:01:00 +0530

    First commit in master branch by Jignesh(as lead) :: FirstClass.java

---
+++ MavenTeamLearningProject/src/test/java/com/java/FirstClass.java
@@ -0,0 +1,10 @@
+package com.java;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am in First Class");
+	}
+
+}
