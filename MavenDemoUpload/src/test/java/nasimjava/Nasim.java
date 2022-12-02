3cfc9c3624f084ea1cfb325dde9457d3f39fa3be
sagar Gidwani <sagar Gidwani@Sagar>
2022-12-02 10:46:43 +0530

    project updated

---
+++ MavenDemoUpload/src/test/java/nasimjava/Nasim.java
@@ -0,0 +1,13 @@
+package nasimjava;
+
+public class Nasim {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("learning GIT by jignesh");
+System.out.println("learning GIT by jignesh");
+System.out.println("learning GIT by jignesh");
+
+	}
+
+}
